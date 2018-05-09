74ac7c2b07c3e66a45c1552b1e66d0c7e201c213
zeaple <137626013@qq.com>
2018-05-09 14:59:10 +0800

    init

---
+++ Marsnony/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Marsnony
+//
+//  Created by Zeaple on 16/2/29.
+//  Copyright © 2016年 Zeaple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Marsnony/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Marsnony
+//
+//  Created by Zeaple on 16/2/29.
+//  Copyright © 2016年 Zeaple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
